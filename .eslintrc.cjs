0f63a46378c2eca7da55c18c0319e51fd9045b23
dttrue <danieltorres@pursuit.org>
2024-04-17 14:31:15 -0400

    First stripe commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true  
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -8,14 +12,27 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect' // Automatically detect the React version
+    }
+  },
+  plugins: [
+    'react-refresh'
+  ],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
   },
-}
+  globals: {
+    process: 'readonly'  
+  }
+};
+
