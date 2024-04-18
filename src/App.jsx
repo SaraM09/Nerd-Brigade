5f44971145413a85c2c369453049e758fed6a676
SaraM09 <sayeramusfika@pursuit.org>
2024-04-17 20:03:19 -0400

    Bookings component first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import BookingForm from '../bookings/BookingsForm';
+const App = () => {
+  const [userId, setUserId] = useState('123');  // Example user ID
+  return (
+    <div>
+      <h1>Booking System</h1>
+      <BookingForm userId={userId} />
+    </div>
+  );
+};
+export default App;
 
-function App() {
-  
 
-  return 
-}
 
-export default App
+
+
+
